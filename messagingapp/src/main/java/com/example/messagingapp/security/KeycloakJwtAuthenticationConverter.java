6ca19843d5b7261716a85ab747c10de38013b98b
MaherKouki <koukimaher386@gmail.com>
2025-01-21 01:09:18 +0100

    commit

---
+++ messagingapp/src/main/java/com/example/messagingapp/security/KeycloakJwtAuthenticationConverter.java
@@ -0,0 +1,12 @@
+package com.example.messagingapp.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+    @Override
+    public AbstractAuthenticationToken convert(Jwt source) {
+        return null;
+    }
+}
