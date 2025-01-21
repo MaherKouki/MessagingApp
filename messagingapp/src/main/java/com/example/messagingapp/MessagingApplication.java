48a670b73886613414f3f15deb4a4e731b266a83
MaherKouki <koukimaher386@gmail.com>
2025-01-21 02:53:27 +0100

    commit
    commit

--- messagingapp/src/main/java/com/example/messagingapp/MessagingApplication.java
+++ messagingapp/src/main/java/com/example/messagingapp/MessagingApplication.java
@@ -2,8 +2,10 @@ package com.example.messagingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MessagingApplication {
 
 	public static void main(String[] args) {
