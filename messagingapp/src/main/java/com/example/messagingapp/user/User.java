986ea86a7c971887e4f15ce6bcce302c5fee52a3
MaherKouki <koukimaher386@gmail.com>
2025-01-21 02:56:49 +0100

    commit
    commit

--- messagingapp/src/main/java/com/example/messagingapp/user/User.java
+++ messagingapp/src/main/java/com/example/messagingapp/user/User.java
@@ -1,4 +1,34 @@
 package com.example.messagingapp.user;
 
-public class User {
+import com.example.messagingapp.common.BaseAuditingEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseAuditingEntity {
+
+
+    @Id
+    private String id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private LocalDateTime lastSeen;
 }
